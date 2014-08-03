864d54441390da8c9226d04d75bf02a3670c1d49
denismosolov <denismosolov@gmail.com>
2014-08-03 23:24:31 +0400

    * Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+Server
+=========
+
+Installation
+--------------
+
+```sh
+npm install node-uuid socket.io express
+```
+
+Run
+--------------
+
+```sh
+nodejs server.js
+```
+
+Test
+---------------
+Open in the browser http://localhost:8081/
+
+Debug
+---------------
+
+```sh
+node-inspector & node --debug server.js
+```
---
+++ server.js
@@ -0,0 +1,45 @@
+var app = require('express')(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server),
+  uuid = require('node-uuid');
+
+var hallRoom  = 'hallRoom';
+
+server.listen(8080);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (client) {
+  // on connect all clients are joined 'hall room'
+  client.join(hallRoom, function () {
+    // then check if 'hall room' contains more than 3 clients
+    if(Object.keys(io.nsps['/'].adapter.rooms[hallRoom]).length >= 3) {
+      // it creates new room and moves 3 clients there
+      var newRoom = uuid.v1(),
+          group3 = Object.keys(io.nsps['/'].adapter.rooms[hallRoom]).slice(0, 4);
+      group3.forEach(function (id) {
+        io.sockets.connected[id].leave(hallRoom);
+        io.sockets.connected[id].join(newRoom);
+        io.sockets.connected[id].room = newRoom;
+      });
+      var host = io.sockets.connected[group3[0]];
+      host.isHost = true;
+      //host.room = newRoom;
+      host.emit('room.created', {room: newRoom});
+      delete newRoom;
+      delete host;
+    }
+  });
+  client.on('room.ready', function () {
+    // just to double check
+    if (client.isHost && client.room) {
+      client.broadcast.to(client.room).emit('room.invite', {room: client.room});
+    }
+  });
+  client.on('disconnect', function () {
+    console.log(client.id + 'disconnected');
+  });
+  client.emit('connection.success', {message: 'Connected. Waiting another players...'});
+});
