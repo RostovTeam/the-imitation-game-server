837ccf8cedd38ea376735dd99f21c14a953b80fc
vladimir <vdaron@gmail.com>
2014-08-04 00:43:48 +0400

    Counter users

--- server.js
+++ server.js
@@ -14,18 +14,24 @@ app.get('/', function (req, res) {
 io.sockets.on('connection', function (client) {
   // on connect all clients are joined 'hall room'
   client.join(hallRoom, function () {
+    
+    
+    io.sockets.in(hallRoom).emit('room.addUser', {users: Object.keys(io.nsps['/'].adapter.rooms[hallRoom]).length});
+
     // then check if 'hall room' contains more than 3 clients
     if(Object.keys(io.nsps['/'].adapter.rooms[hallRoom]).length >= 3) {
       // it creates new room and moves 3 clients there
       var newRoom = uuid.v1(),
           group3 = Object.keys(io.nsps['/'].adapter.rooms[hallRoom]).slice(0, 4);
+
       group3.forEach(function (id) {
         io.sockets.connected[id].leave(hallRoom);
         io.sockets.connected[id].join(newRoom);
         io.sockets.connected[id].room = newRoom;
       });
+
       var host = io.sockets.connected[group3[0]];
-      host.isHost = true;
+      host.isHost = true;  
       //host.room = newRoom;
       host.emit('room.created', {room: newRoom});
       delete newRoom;
