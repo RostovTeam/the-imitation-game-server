607bd5fa9f5da982a3c40e8fd78e8ec58c57430f
denismosolov <denismosolov@gmail.com>
2014-08-10 03:47:02 +0400

    latest

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = require('express')(),
   io = require('socket.io').listen(server),
   uuid = require('node-uuid');
 
-var hallRoom  = 'hallRoom';
+var hallRoomId  = 'hallRoom';
 
 server.listen(8080);
 
@@ -13,35 +13,45 @@ app.get('/', function (req, res) {
 
 io.sockets.on('connection', function (client) {
   // on connect all clients are joined 'hall room'
-  client.join(hallRoom, function () {
-    
-    
-    io.sockets.in(hallRoom).emit('room.addUser', {users: Object.keys(io.nsps['/'].adapter.rooms[hallRoom]).length});
+  client.join(hallRoomId, function () {
+    // how many cients in wait list
+    io.sockets.in(hallRoomId).emit('room.addUser', {users: Object.keys(io.nsps['/'].adapter.rooms[hallRoomId]).length});
 
-    // then check if 'hall room' contains more than 3 clients
-    if(Object.keys(io.nsps['/'].adapter.rooms[hallRoom]).length >= 3) {
-      // it creates new room and moves 3 clients there
-      var newRoom = uuid.v1(),
-          group3 = Object.keys(io.nsps['/'].adapter.rooms[hallRoom]).slice(0, 4);
+    // if 'hall room' contains more than 3 clients
+    if(Object.keys(io.nsps['/'].adapter.rooms[hallRoomId]).length >= 3) {
+      // it creates new room, puts the first 3 clients into new room, removed them from 'hall room'
+      var newRoomId = uuid.v1(),
+          group3 = Object.keys(io.nsps['/'].adapter.rooms[hallRoomId]).slice(0, 4);
 
       group3.forEach(function (id) {
-        io.sockets.connected[id].leave(hallRoom);
-        io.sockets.connected[id].join(newRoom);
-        io.sockets.connected[id].room = newRoom;
+        io.sockets.connected[id].leave(hallRoomId);
+        io.sockets.connected[id].join(newRoomId);
+        io.sockets.connected[id].room = newRoomId;
       });
 
-      var host = io.sockets.connected[group3[0]];
-      host.isHost = true;  
-      //host.room = newRoom;
-      host.emit('room.created', {room: newRoom});
-      delete newRoom;
-      delete host;
+      var seeker = io.sockets.connected[group3[0]],
+        liar = io.sockets.connected[group3[1]],
+        honest = io.sockets.connected[group3[2]];
+
+      seeker.isSeeker = true;
+      liar.isLiar = true;
+      honest.isHonest = true;
+      seeker.emit('webrtc.multiconnection.open', {room: newRoomId});
     }
   });
-  client.on('room.ready', function () {
+  client.on('webrtc.multiconnection.ready', function () {
     // just to double check
-    if (client.isHost && client.room) {
-      client.broadcast.to(client.room).emit('room.invite', {room: client.room});
+    if (client.room && client.isSeeker) {
+      client.broadcast.to(client.room).emit('webrtc.multiconnection.connect', {room: client.room});
+    }
+  });
+  client.on('game.role', function(){
+    if (client.isSeeker) {
+      client.emit('game.role', {role: 'seeker'});
+    } else if (client.isHonest) {
+      client.emit('game.role', {role: 'honest'});
+    } else if (client.isLiar) {
+      client.emit('game.role', {role: 'liar'});
     }
   });
   client.on('game.vote', function() {
